27087f4c3726921bd03d5253a6fc569be30ac94e
Muhammad Fadhil Hidayatullah <fgeminni0@gmail.com>
2025-07-23 14:41:58 +0700

    authorization

---
+++ resources/views/dashboard/categories/index.blade.php
@@ -0,0 +1,67 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+  <div
+    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+  >
+    <h1>Posts Categories</h1>
+  </div>
+
+  @if (session()->has('success'))
+    <div class="alert alert-success col-lg-6" role="alert">
+      {{ session('success') }}
+    </div>
+  @endif
+
+  <div class="table-responsive col-lg-6">
+    <a class="btn btn-success my-2" href="/dashboard/categories/create">
+      <i class="bi bi-plus"></i>
+      Create new category
+    </a>
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th scope="col">No.</th>
+          <th scope="col">Category Name</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($categories as $category)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $category->name }}</td>
+            <td>
+              <a
+                class="badge bg-info"
+                href="/dashboard/categories/{{ $category->slug }}"
+              >
+                <i class="bi bi-eye"></i>
+              </a>
+              <a
+                class="badge bg-warning"
+                href="/dashboard/categories/{{ $category->slug }}/edit"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </a>
+              <form
+                action="/dashboard/categories/{{ $category->slug }}"
+                method="POST"
+                class="d-inline"
+              >
+                @method('delete')
+                @csrf
+                <button
+                  class="badge bg-danger border-0"
+                  onclick="return confirm('Are you sure?')"
+                >
+                  <i class="bi bi-trash3"></i>
+                </button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+@endsection
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -34,6 +34,25 @@
           </a>
         </li>
       </ul>
+
+      @can('admin')
+        <h6
+          class="heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+        >
+          <span>Administrator</span>
+        </h6>
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a
+              class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}"
+              href="/dashboard/categories"
+            >
+              <i class="bi bi-grid"></i>
+              Post Categories
+            </a>
+          </li>
+        </ul>
+      @endcan
     </div>
   </div>
 </nav>
